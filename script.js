1dc13088c1cd6f7e5fd3b65981eb06d4ef8113e2
= <=>
2023-12-09 00:23:18 -0500

    added switch statement to handle input

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-commands = ["whoami",
-    "help", "linkedin", "github", "purpose", "bio"];
+const commands = ["whoami", "help", "linkedin", "github", "purpose", "bio"];
 
-function main(){
-    let command = document.getElementById("command");
-    console.log(is_command(command));
+function main() {
+    let commandForm = document.getElementById("commandForm");
+    commandForm.addEventListener("submit", handleFormSubmit);
 }
 
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-function is_command(command) {
+    let commandInput = document.getElementById("command");
+    let userInput = commandInput.value.trim(); // Get the user input value and trim extra spaces
+    console.log(isCommand(userInput));
+    
+    switch (userInput) {
+        case "whoami":
+            displayWhoami();
+            break;
+        case "linkedin":
+            displayLinkedin();
+            break;
+        case "github":
+            displayGithub();
+            break;
+        case "purpose":
+            displayPurpose();
+            break;
+        case "bio":
+            displayBio();
+            break;
+        case "help":
+            displayHelp();
+            break;
+        default:
+            alert("Invalid command!"); // Notify the user if the entered command is not recognized
+    }
+}
+
+function isCommand(command) {
     return commands.includes(command);
 }
 
-
+document.addEventListener("DOMContentLoaded", main);
