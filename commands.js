d2ae71716771cb484a1171d8992d5f05151ba6d7
= <=>
2023-12-09 00:22:51 -0500

    Moved all command specific logic into commands.js

--- commands.js
+++ commands.js
@@ -1,7 +1,25 @@
-commands = "whoami, help, linkedin, github, purpose, bio,";
-
-function is_command(command) {
-    return command in commands;
+function displayHelp() {
+    console.log("Help command was executed");
 }
 
+function displayWhoami() {
+    console.log("Whoami command was executed");
+}
 
+function displayLinkedin() {
+    console.log("Linkedin command was executed");
+}
+
+function displayGithub() {
+    console.log("Github command was executed");
+}
+
+function displayPurpose() {
+    console.log("Purpose command was executed");
+}
+
+function displayBio() {
+    console.log("Bio command was executed");
+}
+
+    
